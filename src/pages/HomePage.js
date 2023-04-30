7a2829ff45ae13bc655d75da29f44e8e1c7fced9
Regina Varybrus <varybrusr@gmail.com>
2023-04-30 23:47:16 +0100

    img added

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -2,6 +2,7 @@ const HomePage = (props) => {
     return (
         <div className="Home-page">
             <h1>Hello, Redux Toolkit!</h1>
+            <img src="https://repository-images.githubusercontent.com/347723622/92065800-865a-11eb-9626-dff3cb7fef55"></img>
         </div>
     )
 }
